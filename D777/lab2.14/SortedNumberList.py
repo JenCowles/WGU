76f97d9b244d82f9ee842932aa49249af1636eb0
JenCowles <47047223+JenCowles@users.noreply.github.com>
2025-05-20 13:56:42 -0600

    Add files via upload

---
+++ D777/lab2.14/SortedNumberList.py
@@ -0,0 +1,19 @@
+from NumberList import NumberList
+from NumberListNode import NumberListNode
+
+class SortedNumberList(NumberList):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    # Inserts the number into the list in the correct position such that the
+    # list remains sorted in ascending order.
+    def insert(self, number):
+        # TODO: Type your code here
+        pass
+    
+    # Removes the node with the specified number value from the list. Returns
+    # True if the node is found and removed, False otherwise.
+    def remove(self, number):
+        # TODO: Type your code here
+        return False
